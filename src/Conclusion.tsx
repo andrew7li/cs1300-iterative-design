08ddd6c83f2facf822638e078398b849212f0a45
celinaye <celinaye@celinas-mbp.devices.brown.edu>
2022-11-07 16:17:42 -0500

    added discussion

--- src/Conclusion.tsx
+++ src/Conclusion.tsx
@@ -5,9 +5,7 @@ export default function Conclusion() {
     <section id="conclusion">
       <h1>Conclusion</h1>
       <p>
-        After completing the iterative design process, we were able to create a
-        final version of our mockups. We then sent our mockups to Pyrls
-        themselves. The lessons we learned from this process were:
+      Through this project, we learned about the iterative process that comes with outlining and creating mockups. By taking user perspectives, we were able to make necessary updates to our mockups and improve the experience of our app. The biggest changes we made from user feedback included increasing user awareness of where they were in the app and creating more consistent stylistic choices. As a result of these changes, we were able to update our high-fidelity mockup to a final version, ready for user testing and to send to Pyrls for company feedback.
       </p>
     </section>
   );
